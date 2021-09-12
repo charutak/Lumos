729ffea5e0a926df7fba5655438cd69680f57861
soumyadeep <soumyadeep9@gmail.com>
2021-09-12 23:37:19 +0530

    Add splash screen

--- lumos-app/src/App.tsx
+++ lumos-app/src/App.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Lobster } from '@expo-google-fonts/lobster';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Lumos!!!!!!!</Text>
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        'Lobster': require('../assets/fonts/Lobster-Regular.ttf'),
+    });
+    const image = require('../assets/img/flashlight_on.png');
+    return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Lumos</Text>
+                <Image source={image} style={styles.flashlight} />
+            </View>
+        );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      flexDirection:'column',
+      backgroundColor: '#272727',
+      alignItems: 'center',
   },
+    title: {
+        color: '#FF9900',
+        fontFamily: 'Lobster',
+        fontSize: 64,
+        paddingTop: 290,
+        paddingBottom: 100
+    },
+    flashlight: {
+        width: 100,
+        height: 100,
+    }
 });
+
