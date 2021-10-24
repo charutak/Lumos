7d76cb6c8000d45075e8f0867767377847cd6708
soumyadeep <soumyadeep9@gmail.com>
2021-10-24 23:44:05 +0530

    Button sending request

--- lumos-app/src/App.tsx
+++ lumos-app/src/App.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import { useFonts, Lobster } from '@expo-google-fonts/lobster';
+import { StyleSheet, Text, View, Image, Button} from 'react-native';
+import { useFonts } from 'expo-font';
+import { AppLoading, Font } from 'expo'
+import ServerAPI from './api/Server';
 
-
-
-export default function App() {
-    let [fontsLoaded] = useFonts({
-        'Lobster': require('../assets/fonts/Lobster-Regular.ttf'),
-    });
-    const image = require('../assets/img/flashlight_on.png');
-    return (
+export default class App extends React.Component {
+    /* private [fontsLoaded] = useFonts({
+     *     'Lobster': require('../assets/fonts/Lobster-Regular.ttf'),
+     * }); */
+    private handleClick = () => {
+        ServerAPI.postRGB();
+        console.log('pressed');
+    };
+    render() {
+        const image = require('../assets/img/flashlight_on.png');
+        return (
             <View style={styles.container}>
                 <Text style={styles.title}>Lumos</Text>
                 <Image source={image} style={styles.flashlight} />
+                <Button title="boo" onPress={this.handleClick}/>
             </View>
         );
+    }
 }
 
+
+/* export default function App() {
+ *     let [fontsLoaded] = useFonts({
+ *         'Lobster': require('../assets/fonts/Lobster-Regular.ttf'),
+ *     });
+ * 
+ *     if (!fontsLoaded) {
+ *         return <AppLoading />;
+ *     } else {
+ *         return (
+ *             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+ *                 <Text style={{ fontFamily: 'Inter-Black', fontSize: 40 }}>Inter Black</Text>
+ *                 <Text style={{ fontSize: 40 }}>Platform Default</Text>
+ *             </View>
+ *         );
+ *     }
+ * };
+ *  */
 const styles = StyleSheet.create({
   container: {
       flex: 1,
